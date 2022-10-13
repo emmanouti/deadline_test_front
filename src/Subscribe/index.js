5499e030dcbd54822b64c3b026d41c2ebaac8664
Emma <emma.nouti@gmail.com>
2022-10-13 17:35:44 +0200

    feat/subscribeView: first view, handle input change, added a theme palette for future style

---
+++ src/Subscribe/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubscribingFormView from "./SubscribingForm.View";
+
+const Subscribe = () => {
+    return (
+       <>
+           <SubscribingFormView />
+       </>
+    )
+}
+
+export default Subscribe;
